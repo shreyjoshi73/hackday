e1051f054a484087a21ab5500bc4e6b948c3e949
shreyjoshi73 <joshishrey30@gmail.com>
2025-11-08 12:49:27 +0530

    Update db.js

--- admission-helper-extension/db.js
+++ admission-helper-extension/db.js
@@ -1,1 +1,30 @@
+// Wrapper around IndexedDB for larger form data and room info.
+const DB_NAME = "AdmissionHelperDB";
+const STORE_NAME = "formData";
+
+export function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, 1);
+    request.onupgradeneeded = e => {
+      const db = e.target.result;
+      db.createObjectStore(STORE_NAME, { keyPath: "id" });
+    };
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+export async function saveData(id, data) {
+  const db = await openDB();
+  const tx = db.transaction(STORE_NAME, "readwrite");
+  tx.objectStore(STORE_NAME).put({ id, ...data });
+  await tx.done;
+}
+
+export async function getData(id) {
+  const db = await openDB();
+  const tx = db.transaction(STORE_NAME, "readonly");
+  const result = await tx.objectStore(STORE_NAME).get(id);
+  return result;
+}
 
