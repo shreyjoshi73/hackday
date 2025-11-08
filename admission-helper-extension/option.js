fc033b86063b499ccf680d5e7e833755a9679408
shreyjoshi73 <joshishrey30@gmail.com>
2025-11-08 14:03:49 +0530

    Delete admission-helper-extension/option.js

--- admission-helper-extension/option.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("userForm").addEventListener("submit", e => {
-  e.preventDefault();
-  const data = Object.fromEntries(new FormData(e.target).entries());
-  chrome.storage.sync.set({ userData: data }, () => alert("✅ Details saved!"));
-});
